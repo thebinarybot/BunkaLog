6bd42d9dc23968c5c3bff41061c51ae38d69a8af
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-03-26 03:17:38 +0530

    Add fetch data from API server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import AttendanceCard from './components/AttendanceCard.js';
 import { Row, Col } from 'reactstrap';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      data: {"data": [{
+        "courseName": "Loading",
+        "courseCode": "Loading",
+        "classesAttended": "Loading",
+        "totalClasses": "Loading"
+        }]
+      },
+    };
+    this.loadData();
+  }
+
+  loadData() {
+    axios.get("http://192.168.86.129:5000/")
+        .then( (response) => {
+            this.setState({data: response});
+        })
+        .catch( (error) => {
+            console.log(error);
+        })
+  }
+
+	render()
+	{
+          var renderData = this.state.data["data"]
+          var cards = renderData.map(function(course, index) {
+            return (
+                <Col>
+                  <AttendanceCard courseName={course["courseName"]}
+                    courseCode={course["courseCode"]}
+                    classesAttended={course["classesAttended"]}
+                    totalClasses={course["totalClasses"]} />
+                 </Col>
+            );
+          })
+
   return (
     <div className="App">
       <NavBar />
-      <Row>
-        <Col>
-          <AttendanceCard courseName="Game Theory" courseCode="15XT81"
-            classesAttended="21" totalClasses="24" />
-        </Col>
-        <Col>
-          <AttendanceCard courseName="Parallel & Distributed Computing" courseCode="15XT82"
-            classesAttended="13" totalClasses="14" />
-        </Col>
+      <Row sm="3">
+          {cards}
       </Row>
+      <p> {this.state.data["data"]["courseName"]} </p>
     </div>
   );
+	}
 }
 
 export default App;
