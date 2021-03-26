f987b517f6e0c89d667b60791f02fa4fffd99f84
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-03-26 13:53:19 +0530

    Fix bugs in API server

--- server.py
+++ server.py
@@ -6,27 +6,27 @@ from flask import Flask, request
 app = Flask(__name__)
 
 data = [
-		{'courseName': 'Game Theory',
-		'courseCode': '15XT81',
-		'classesAttended': 21,
-		'totalClasses': 24},
-		{'courseName': 'Parallel & Distributed Computing',
-		'courseCode': '15XT82',
-		'classesAttended': 13,
-		'totalClasses': 14},
-		{'courseName': 'Mathematical Modelling',
-		'courseCode': '15XT83',
-		'classesAttended': 20,
-		'totalClasses': 28},
-		{'courseName': 'ADBMS',
-		'courseCode': '15XTE1',
-		'classesAttended': 14,
-		'totalClasses': 18},
-		{'courseName': 'DV',
-		'courseCode': '15XTOK',
-		'classesAttended': 22,
-		'totalClasses': 22}
-	]
+                {'courseName': 'Game Theory',
+                'courseCode': '15XT81',
+                'classesAttended': 21,
+                'totalClasses': 24},
+                {'courseName': 'Parallel & Distributed Computing',
+                'courseCode': '15XT82',
+                'classesAttended': 13,
+                'totalClasses': 14},
+                {'courseName': 'Mathematical Modelling',
+                'courseCode': '15XT83',
+                'classesAttended': 20,
+                'totalClasses': 28},
+                {'courseName': 'ADBMS',
+                'courseCode': '15XTE1',
+                'classesAttended': 14,
+                'totalClasses': 18},
+                {'courseName': 'DV',
+                'courseCode': '15XTOK',
+                'classesAttended': 22,
+                'totalClasses': 22}
+        ]
 
 @app.route('/')
 def index():
@@ -34,30 +34,32 @@ def index():
 
 @app.route('/add-class', methods=['GET'])
 def addClass():
-	name =	request.form.get('courseName')
-	idval = request.form.get('couseCode')
-	attend = request.form.get('classesAttended')
-	total = request.form.get('totalClasses')
-	newdict = {'courseName' : name,
-				'courseCode' : idval,
-				'classesAttended' : attend,
-				'totalClasses' : total}
-	data.append(newdict) 
+        name =        request.form.get('courseName')
+        idval = request.form.get('couseCode')
+        attend = request.form.get('classesAttended')
+        total = request.form.get('totalClasses')
+        newdict = {'courseName' : name,
+                                'courseCode' : idval,
+                                'classesAttended' : attend,
+                                'totalClasses' : total}
+        data.append(newdict) 
 
 @app.route('/attend-class', methods=['GET'])
 def attendClass():
-	classVal = request.form.get('courseName')
-	for i in data:
-		if(i['couseName']==classVal):
-			i['classAttended']==i['classAttended']+1
-			i['totalClasses']==i['totalClasses']+1
+        classVal = request.args.get('courseCode')
+        for i in data:
+                if(i['courseCode']==classVal):
+                        i['classesAttended']=i['classesAttended']+1
+                        i['totalClasses']=i['totalClasses']+1
+        return '<script>window.location.replace("http://192.168.86.129:3000/")</script>'
 
 @app.route('/bunk-class', methods=['GET'])
 def bunkClass():
-	classVal = request.form.get('courseName')
-	for i in data:
-		if(i['couseName']==classVal):
-			i['totalClasses']==i['totalClasses']+1
+        classVal = request.args.get('courseCode')
+        for i in data:
+                if(i['courseCode']==classVal):
+                        i['totalClasses']=i['totalClasses']+1
+        return '<script>window.location.replace("http://192.168.86.129:3000/")</script>'
 
 if __name__=='__main__':
-	app.run(host="192.168.86.129")
+        app.run(host="192.168.86.129")
