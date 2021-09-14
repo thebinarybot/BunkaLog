e3ec415d6b6a223f36d2a0b01cb459d1462ec630
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-09-14 20:03:03 +0530

    Refactor AddClassForm to prevent redirect on submit

--- src/components/AddClassForm.js
+++ src/components/AddClassForm.js
@@ -1,31 +1,82 @@
 import React from 'react';
 import config from '../config.js';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class AddClass extends React.Component {
+class AddClass extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      courseName: '',
+      courseCode: '',
+      classesAttended: 0,
+      totalClasses: 0,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event)
+  {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event)
+  {
+    const requestOptions = {
+      method: 'POST',
+      headers: {'bunkalog_session_id': Cookies.get('bunkalog_session_id')},
+      url: config.PROXY_URL + '/add-class',
+      data: this.state,
+    }
+
+    axios(requestOptions)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    event.preventDefault();
+  }
+
   render() {
   var formUrl = config.PROXY_URL + '/add-class';
   return (
-    <Form action = {formUrl} >
+    <Form onSubmit={this.handleSubmit} >
 
       <FormGroup>
         <Label for="courseName">Course Name</Label>
-        <Input type="text" name="courseName" id="courseName" />
+        <Input type="text" name="courseName" value={this.state.courseName}
+          id="courseName" onChange={this.handleChange} />
       </FormGroup>
 
       <FormGroup>
         <Label for="courseCode">Course Code</Label>
-        <Input type="text" name="courseCode" id="courseCode" />
+        <Input type="text" name="courseCode" value={this.state.courseCode}
+          id="courseCode" onChange={this.handleChange} />
       </FormGroup>
 
       <FormGroup>
         <Label for="classesAttended">Classes Attended</Label>
-        <Input type="number" name="classesAttended" id="classesAttended" />
+        <Input type="number" name="classesAttended" value={this.state.classesAttended}
+          id="classesAttended" onChange={this.handleChange} />
       </FormGroup>
 
       <FormGroup>
         <Label for="totalClasses">Total Classes</Label>
-        <Input type="number" name="totalClasses" id="totalClasses" />
+        <Input type="number" name="totalClasses" value={this.state.totalClasses}
+          id="totalClasses" onChange={this.handleChange} />
       </FormGroup>
 
       <Button type="Submit">
